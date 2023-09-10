f8d7b6e81aa71ccfbeac4cb26ddd806deab78cc3
rhuaannn <rhuanmarques1223@gmail.com>
2023-09-10 15:27:49 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,82 @@
+//script 1
+alert("Hello World!")
+
+
+//script 2
+const numberOne = 2
+const NumberTwo = 3
+
+let result = Number(numberOne) + Number(NumberTwo)
+
+console.log(result)
+
+
+//script 3
+
+const verificationVariable = "4"
+
+if(typeof verificationVariable === "number"){
+    console.log("É um numero")
+}else{
+    console.log("Não é um número")
+}
+
+ //script 4
+
+ const verificationVariableTwo = "4"
+
+if(typeof verificationVariableTwo === "string"){
+    console.log("É uma string")
+}else{
+    console.log("Não é uma string")
+}
+
+
+// script 5
+
+const verificationVariableThree = true
+
+if(typeof verificationVariableThree === "boolean"){
+    console.log("É uma boolean")
+}else{
+    console.log("Não é uma boolean")
+}
+
+//script 6
+
+const numberSub = 10
+const numberSubTwo = 4
+
+let total = Number(numberSub) - Number(numberSubTwo)
+
+console.log(total)
+
+
+//script 7
+const numberMult = 4
+const numberMultTwo = 4
+let resultMult = Number(numberMult) * Number(numberMultTwo);
+console.log(resultMult)
+
+//script 8
+const numberDivision = 10
+const numberDivisionTwo = 2
+let resultDivision = Number(numberDivision) / Number(numberDivisionTwo)
+console.log(resultDivision)
+
+
+//script 9
+const parOfImpar = 11
+
+let resultParOfImpar = parOfImpar % 2 === 0 ? "numero par " : "Numero impar"
+
+console.log(resultParOfImpar)
+
+
+///script 10
+
+const imparOfPar = 11
+
+let resultImparOfPar = imparOfPar % 2 === 0 ? "numero par " : "Numero impar"
+
+console.log(resultParOfImpar)
